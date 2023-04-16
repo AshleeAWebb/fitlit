ac1271a61a9bdbf51eb330bb35574e6e7aac0c32
Rachel Brendel <brendelrachel@gmail.com>
2023-04-15 22:14:31 -0500

    Add file for motivational sayings

---
+++ src/data/motivation-data.js
@@ -0,0 +1,9 @@
+const motivationSayings = [
+  "Your Failure is not final! It is the courage to continue that counts",
+  "The best view comes after the hardest climb.",
+  "BIG journeys begin with SMALL steps.",
+  "Fear kills more dreams than failure ever will!",
+  "Optimism is a happiness magnet. If you stay positive, good things and good people will be drawn to you!",
+  "Everyday is another chance to be better!",
+  "Balance is not something you find. It's something that you create."
+];
