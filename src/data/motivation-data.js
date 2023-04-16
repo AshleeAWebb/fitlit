64575e14fb2b9f848625c99b35abc004b689e299
Rachel Brendel <brendelrachel@gmail.com>
2023-04-15 22:41:55 -0500

    Add different sayings for each level of motivation

--- src/data/motivation-data.js
+++ src/data/motivation-data.js
@@ -1,9 +1,29 @@
-const motivationSayings = [
-  "Your Failure is not final! It is the courage to continue that counts",
+const highMotivationSayings = [
   "The best view comes after the hardest climb.",
-  "BIG journeys begin with SMALL steps.",
-  "Fear kills more dreams than failure ever will!",
   "Optimism is a happiness magnet. If you stay positive, good things and good people will be drawn to you!",
-  "Everyday is another chance to be better!",
-  "Balance is not something you find. It's something that you create."
+  "Being happy is the greatest form of success!",
+  "Happiness is contagious, spread this among people surrounding you!!!",
+  "Because of your smile, you make life more beautiful!",
+  "You glow differently when you are HAPPY!!",
+  "If you are happy, then nothing else matters."
+];
+
+const mediumMotivationSayings = [
+  "BIG journeys begin with SMALL steps.",
+  "Balance is not something you find. It's something that you create.",
+  "The comeback is always stronger than the setback.",
+  "The struggle you're in today is developing the stregth you need for tomorrow.",
+  "You are BRAVE. You are AMAZING. You are STRONG.",
+  "Smile, because today you are a lot stronger than you were yesterday.",
+  "One small positive thought in the morning can change your whole day."
+];
+
+const lowMotivationSayings = [
+  "Your Failure is not final! It is the courage to continue that counts.",
+  "Fear kills more dreams than failure ever will!",
+  "You only fail when you give up.",
+  "Never give up... Better days are on the way.",
+  "When it rains, look for rainbows. When it's dark, look for stars.",
+  "Just for the record darling, not all positive change feels positive in the beginning.",
+  "It may be stormy now, but it never rains forever. BE STRONG!"
 ];
